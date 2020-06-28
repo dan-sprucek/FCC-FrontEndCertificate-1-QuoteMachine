b5536dfe2149b2b9d95aea3c375855a52eeec172
Dan S <63663822+Phoenka@users.noreply.github.com>
2020-06-28 12:14:47 +0200

    Updates

---
+++ precache-manifest.218c6107fcad1e9e7b8577a5f6ed3f26.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9b3cb3301c95c53e56bcaec0cc9f5a28",
+    "url": "/First-React-App/index.html"
+  },
+  {
+    "revision": "b1df6d45767d56789d8f",
+    "url": "/First-React-App/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "6a10a78df6c76ca3a627",
+    "url": "/First-React-App/static/js/2.b1b31c97.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/First-React-App/static/js/2.b1b31c97.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b1df6d45767d56789d8f",
+    "url": "/First-React-App/static/js/main.2072ad64.chunk.js"
+  },
+  {
+    "revision": "0501366d9c31de98c2f7",
+    "url": "/First-React-App/static/js/runtime-main.553464fc.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/First-React-App/static/media/logo.5d5d9eef.svg"
+  }
+]);
